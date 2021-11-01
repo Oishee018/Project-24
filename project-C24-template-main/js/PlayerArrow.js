68881b1f2a4a73e29785237a94a126d5076e3380
Oishee018 <88925592+Oishee018@users.noreply.github.com>
2021-11-01 13:39:16 +0000

    Add files via upload

---
+++ project-C24-template-main/js/PlayerArrow.js
@@ -0,0 +1,36 @@
+class PlayerArrow {
+  constructor(x, y, width, height, archerAngle) {
+    var options = {
+      isStatic: true,
+      density: 0.1
+    };
+    this.width = width;
+    this.height = height;
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+    this.image = loadImage("./assets/arrow.png");
+    this.archerAngle = archerAngle;
+    this.velocity = 0;
+    World.add(world, this.body);
+  }
+
+  if (keyCode === 32) {
+    arrow.shoot(playerArcher.body.angle);
+  }
+  
+  //create a shoot function
+ shoot()
+ archerAngle * (3.14/180)
+
+  display() {
+  
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
